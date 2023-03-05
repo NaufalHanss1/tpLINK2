47679511969290c3e75b6cef383db63de107c198
NaufalHanss <127005909+NaufalHanss@users.noreply.github.com>
2023-03-05 13:36:22 +0700

    Aselebew

---
+++ TPmod2KPL/Form1.cs
@@ -0,0 +1,26 @@
+namespace TPmod2KPL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputText = textBox1.Text;
+
+         
+            string outputText = "Hello, " + inputText + "!";
+
+     
+            label1.Text = outputText;
+        }
+    }
+}
